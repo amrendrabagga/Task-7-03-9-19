452c70c91aafa7070866de8566afcd6c40f2b63c
Amrendra Bagga <amrendrabagga@gmail.com>
2019-09-04 10:56:34 +0530

    updating CRUDOperation.java

--- src/crud/jdbc/CRUDOperation.java
+++ src/crud/jdbc/CRUDOperation.java
@@ -10,42 +10,51 @@ import java.sql.Statement;
 
 public class CRUDOperation {
 
-	public static void main(String[] args) throws ClassNotFoundException, SQLException, NumberFormatException, IOException {
+	public static void main(String[] args)
+			throws ClassNotFoundException, SQLException, NumberFormatException, IOException {
 		Class.forName("com.mysql.cj.jdbc.Driver");
 		Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/wp", "root", "root123@");
 		Statement s = con.createStatement();
-		System.out.println("1.insert 2.update 3.delete 4.add column designation");
+		System.out.println("1.create table 2.insert 3.update 4.delete 5.add column designation");
 		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 		int choice = Integer.parseInt(reader.readLine());
-		
-		switch(choice) {
+
+		switch (choice) {
+
 		case 1:
+			String createQuery = "create table emp(eno int primary key,ename varchar(25),salary int)";
+			boolean checkCreate = s.execute(createQuery);
+
+			System.out.println("EMP TABLE CREATED");
+			break;
+		case 2:
 			String insertQuery = "insert into emp values(111,'amrendra',100000)";
 			int checkInsert = s.executeUpdate(insertQuery);
-			if(checkInsert!=0)
+			if (checkInsert != 0)
 				System.out.println("1 ROW INSERTED SUCCESSFULLY");
 			break;
-			
-		case 2:
+
+		case 3:
 			String updateQuery = "update emp set salary = 110000 where eno=111";
 			int checkUpdate = s.executeUpdate(updateQuery);
-			if(checkUpdate!=0)
+			if (checkUpdate != 0)
 				System.out.println("1 ROW UPDATED SUCCESSFULLY");
 			break;
-		
-		case 3:
+
+		case 4:
 			String deleteQuery = "delete from emp where eno=111";
 			int checkDelete = s.executeUpdate(deleteQuery);
-			if(checkDelete!=0)
+			if (checkDelete != 0)
 				System.out.println("1 ROW DELETED SUCCESSFULLY");
 			break;
-			
-		case 4:
-			String  alterQuery = "alter table emp add designation varchar(10)";
-			int checkAlter = s.executeUpdate(alterQuery);
-			if(checkAlter!=0)
-				System.out.println("TABLE ALTERED SUCCESSFULLY");
+
+		case 5:
+			String alterQuery = "alter table emp add designation varchar(10)";
+			s.executeUpdate(alterQuery);
+
+			System.out.println("TABLE ALTERED SUCCESSFULLY");
 			break;
+
 		default:
 			System.out.println("CHOOSE CORRECT OPTION");
 		}
